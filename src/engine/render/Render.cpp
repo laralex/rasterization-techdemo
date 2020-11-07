8298a07f1a466b51468e77cb65885cda1d1e6c23
Alex Larionov <alxs.larionov@gmail.com>
2020-11-08 00:25:17 +0300

    Add glfw library (git submodule): integrated, but untested

--- src/engine/render/Render.cpp
+++ src/engine/render/Render.cpp
@@ -1,5 +1,9 @@
 #include "Render.hpp"
 #include "engine/core/Core.hpp"
+
+#define GLFW_INCLUDE_NONE
+#include "GLFW/glfw3.h"
+
 namespace engine::render {
     std::int32_t echo(std::int32_t message) {
         auto answer = engine::core::echo(message);
