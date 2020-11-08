94a1f8c24cb3ddd6680154f7cefe2f13006534f4
Alex Larionov <alxs.larionov@gmail.com>
2020-11-08 11:30:07 +0300

    Add test for OpenGL basic initialization

--- test/example_test.cpp
+++ test/example_test.cpp
@@ -6,3 +6,9 @@ GTEST_TEST(Example, Echo)
     auto res = engine::render::echo(42);
     ASSERT_EQ(res, 42);
 }
+
+GTEST_TEST(Render, OpenGL)
+{
+    auto succeeded = engine::render::initialize_opengl();
+    ASSERT_EQ(succeeded, 0);
+}
