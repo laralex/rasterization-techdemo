59e26d3589a18d1959413c9c660d42a99244a5fd
Alex Larionov <alxs.larionov@gmail.com>
2020-11-08 11:29:18 +0300

    Add glad library (manually); integrated together with glfw

--- src/engine/render/Render.cpp
+++ src/engine/render/Render.cpp
@@ -1,13 +1,45 @@
 #include "Render.hpp"
 #include "engine/core/Core.hpp"
 
-#define GLFW_INCLUDE_NONE
+//#define GLFW_INCLUDE_NONE
+#include "glad/glad.h"
 #include "GLFW/glfw3.h"
+#include "spdlog/spdlog.h"
 
-namespace engine::render {
-    std::int32_t echo(std::int32_t message) {
-        auto answer = engine::core::echo(message);
-        return answer;
-    }
+namespace engine::render 
+{
+    
+ENGINE_API std::int32_t echo(std::int32_t message) {
+    auto answer = engine::core::echo(message);
+    return answer;
 }
+
+ENGINE_API std::int32_t initialize_opengl() {
+    glfwInit();
+    glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
+    glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
+    glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
+    //glfwWindowHint(GLFW_OPENGL_FORWARD_COMPAT, GL_TRUE);
+
+    GLFWwindow* window = glfwCreateWindow(800, 600, "LearnOpenGL", NULL, NULL);
+    if (window == NULL)
+    {
+        spdlog::info("Failed to create GLFW window");
+        glfwTerminate();
+        return -1;
+    }
+    glfwMakeContextCurrent(window);
+
+    
+    if (!gladLoadGLLoader((GLADloadproc)glfwGetProcAddress))
+    {
+        spdlog::info("Failed to initialize GLAD");
+        return -1;
+    } 
+
+    glfwTerminate();
+    return 0;
+}
+
+} // namespace engine::render
 
--- src/engine/render/Render.hpp
+++ src/engine/render/Render.hpp
@@ -1,5 +1,7 @@
 #include "engine/core/Commons.h"
 #include <cstdint>
-namespace engine::render {
+namespace engine::render 
+{
     ENGINE_API std::int32_t echo(std::int32_t message);
+    ENGINE_API std::int32_t initialize_opengl();
 }
